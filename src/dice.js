2af6d42e7f27d8a0a0bd1326e158ed96416aaf2f
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-11 10:05:43 -0700

    Fix indentations and remove comments and console logs.

--- src/dice.js
+++ src/dice.js
@@ -1,7 +1,7 @@
 //Buisness Logic for Game
 
-function Game(){
-  this.gamePlayers = {}
+export function Game(){
+  this.gamePlayers = {};
   this.currentId = 1;
   this.maxId = 0;
 }
@@ -12,29 +12,23 @@ Game.prototype.addPlayer = function(player){
 };
 
 Game.prototype.assignId = function(){
-//  this.currentId += 1;
+
   this.maxId += 1;
   return this.maxId;
 };
 
 Game.prototype.findPlayer = function(){
-  // if(this.gamePlayers[id] !== undefined){
-  //   return this.addPlayer[id];
-  // } else {
-  //   return this.addPlayer[0];
-  // }
+
   let currentPlayer;
   Object.keys(this.gamePlayers).forEach(function(key) {
-    //  console.log("key",key,typeof(gameToDisplay.currentId),gameToDisplay.currentId.toString() ===  key)
-    if(game.currentId.toString() ===  key){
+    if(Game.currentId.toString() ===  key){
       currentPlayer = this.gamePlayers[key];
     }
   });
   return currentPlayer;
-}
+};
 
 Game.prototype.updateCurrentId = function(){
-  console.log("type current + max:",typeof(this.currentId),typeof(this.maxId));
   if(this.currentId >= this.maxId){
     this.currentId = 1;
   } else {
@@ -43,30 +37,19 @@ Game.prototype.updateCurrentId = function(){
 };
 
 //Business Logic for Players
-function Player(playerName) {
+export function Player(playerName) {
   this.playerName = playerName;
   this.totalScore = 0;  
   this.roundScore = 0;
   this.roll = 0;
-  //this.scoreHistory = [];
 }
 
 //Business Logic for Player Turns
 
 Player.prototype.playerTurn = function() {
   this.rollDice();
-  console.log(this.roll,this.roundScore);
-  // if (this.roll === 1) {
-  //   this.roundScore = 0;
-  //   this.holdDice();
-  //   console.log("roll = 0, pass turn");
-  // } else  {
-  //   this.roundScore += this.roll;
-  // }
+
   this.roundScore += this.roll;
- // this.checkWin();
-  
-  // call function to show updated score
 };
 
 Player.prototype.rollDice = function() {
@@ -84,9 +67,3 @@ Player.prototype.holdDice = function(){
   this.roundScore = 0;
   this.roll = 0;
 };
-
-// Player.prototype.passTurn = function() {
-
-//   // WIP
-//   // update database of palyers to go to next player in the list 
-// };
