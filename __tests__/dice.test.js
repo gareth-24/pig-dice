fa893b8f35dc826fbf69b68633e323a84dcf4113
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-11 11:21:53 -0700

    Add initial jest test for the game and player constructors.

---
+++ __tests__/dice.test.js
@@ -0,0 +1,22 @@
+import {Game, Player} from "./../src/dice.js";
+
+describe('Game', () => {
+
+  test('should correctly create a game object with no players', () => {
+    const game = new Game();
+    expect(game.gamePlayers).toEqual({});
+    expect(game.currentId).toEqual(1);
+    expect(game.maxId).toEqual(0);
+  });
+});
+
+describe('Player', () => {
+
+  test('should correctly create a player object with a name and intial scores of 0', () => {
+    const player = new Player("player1");
+    expect(player.playerName).toEqual("player1");
+    expect(player.totalScore).toEqual(0);
+    expect(player.roundScore).toEqual(0);
+    expect(player.roll).toEqual(0);
+  });
+});
